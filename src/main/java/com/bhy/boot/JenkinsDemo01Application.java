c1ec5402caf940b46b9b260800c41ebc7e58c125
admin_bhy <736143842@qq.com>
2023-01-10 22:56:40 +0800

    jenkins commit01

---
+++ src/main/java/com/bhy/boot/JenkinsDemo01Application.java
@@ -0,0 +1,13 @@
+package com.bhy.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemo01Application.class, args);
+    }
+
+}
